2d6ca615bd630a2f94c6d6df15513a881779f319
Tirtanusa Kurnia Adhi Perdana <98889173+tirtanusa@users.noreply.github.com>
2023-12-14 14:26:56 +0700

    Update Date and Time Mechanism

--- app/src/main/java/com/example/projekpmob_bagianmain/Report.kt
+++ app/src/main/java/com/example/projekpmob_bagianmain/Report.kt
@@ -3,4 +3,5 @@ package com.example.projekpmob_bagianmain
 data class Report(val headerReport : String = "",
                   val isiReport : String = "",
                   val user: String ="",
-                  val timestamp: String ="")
+                  val timestamp: String ="",
+                  val status: Boolean = false)
