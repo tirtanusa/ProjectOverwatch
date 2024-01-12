4bd6111aecf49446f34a6ae00ee875055b31d163
Tirtanusa Kurnia Adhi Perdana <98889173+tirtanusa@users.noreply.github.com>
2024-01-12 21:34:38 +0700

    perbaiki tampilan di fragment update dan post

--- app/src/main/java/com/example/projekpmob_bagianmain/Report.kt
+++ app/src/main/java/com/example/projekpmob_bagianmain/Report.kt
@@ -1,6 +1,7 @@
 package com.example.projekpmob_bagianmain
 
 data class Report(
+    //variable di report
     val headerReport: String = "",
     val isiReport: String = "",
     val user: String ="",
