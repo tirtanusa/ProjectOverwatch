6c159348820f78d5d09f7f806560d7b9c7f7f269
Tirtanusa Kurnia Adhi Perdana <98889173+tirtanusa@users.noreply.github.com>
2024-07-08 12:53:30 +0700

    Update on name, icon and (login, forget and register screen)

--- app/src/main/java/com/example/projekpmob_bagianmain/ReportFragment.kt
+++ app/src/main/java/com/example/projekpmob_bagianmain/ReportFragment.kt
@@ -1,15 +1,12 @@
 package com.example.projekpmob_bagianmain
 
 // Import statements
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,7 +15,6 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.ArrayList
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.firebase.firestore.Query
 
 class ReportFragment : Fragment() {
@@ -45,61 +41,71 @@ class ReportFragment : Fragment() {
     }
 
     private fun getNotificationsFromFirestore() {
-        val firestore = Firebase.firestore
-        firestore.collection("report")
-            .orderBy("timestamp", Query.Direction.DESCENDING)
-            .get()
-            .addOnSuccessListener { documents ->
-                if (isAdded) { // Periksa apakah fragment masih terhubung ke activity
-                    val notificationList = ArrayList<Report>()
-                    for (document in documents) {
-                        val notification = document.toObject(Report::class.java)
-                        notificationList.add(notification)
+        val sharedPreferences = requireContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val emailShared = sharedPreferences.getString("EMAIL", null)
+
+        if (emailShared != null) {
+            val firestore = Firebase.firestore
+            firestore.collection("report")
+                .whereEqualTo("user", emailShared.toString()) // Filter by email
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .get()
+                .addOnSuccessListener { documents ->
+                    if (isAdded) { // Check if fragment is still attached to activity
+                        val notificationList = ArrayList<Report>()
+                        for (document in documents) {
+                            val notification = document.toObject(Report::class.java)
+                            notificationList.add(notification)
+                        }
+
+                        updateRecyclerView(notificationList)
                     }
-
-                    updateRecyclerView(notificationList)
                 }
-            }
-
+                .addOnFailureListener { exception ->
+                    Log.w("ReportFragment", "Error getting documents: ", exception)
+                    Toast.makeText(context, "Failed to load reports.", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            Toast.makeText(context, "No email found in shared preferences.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun goToPostReportFragment() {
-        // Ganti ReportFragment dengan PostReportFragment
+        // Replace ReportFragment with PostReportFragment
         parentFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, PostReportFragment())
             .addToBackStack(null)
             .commit()
-        Log.d("Report Fragment","Fragment Post Kebuka")
+        Log.d("ReportFragment", "PostReportFragment opened")
     }
 
     private fun updateRecyclerView(notificationList: List<Report>) {
-        binding?.let {
+        binding.let {
             val adapter = it.recyclerViewReport.adapter as? NotificationAdapter
             adapter?.updateData(notificationList)
         }
     }
 
     private fun setupRecyclerView() {
-            binding.recyclerViewReport.layoutManager = LinearLayoutManager(context)
-            binding.recyclerViewReport.adapter = NotificationAdapter(emptyList()) { headerReport, isiReport,docID ->
-                // Implementasi aksi yang diinginkan ketika item diklik
-                // Misalnya, navigasi ke UpdateReportFragment dengan data yang diperlukan
-                val bundle = Bundle().apply {
-                    putString("HEADER_REPORT", headerReport)
-                    putString("ISI_REPORT", isiReport)
-                    putString("DOCUMENT_ID", docID)
-                }
-                val updateReportFragment = UpdateReportFragment().apply {
-                    arguments = bundle
-                }
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, updateReportFragment)
-                    .addToBackStack(null)
-                    .commit()
+        binding.recyclerViewReport.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewReport.adapter = NotificationAdapter(emptyList()) { headerReport, isiReport, docID ->
+            // Implement desired action when an item is clicked
+            // For example, navigate to UpdateReportFragment with the necessary data
+            val bundle = Bundle().apply {
+                putString("HEADER_REPORT", headerReport)
+                putString("ISI_REPORT", isiReport)
+                putString("DOCUMENT_ID", docID)
             }
-            getNotificationsFromFirestore()
+            val updateReportFragment = UpdateReportFragment().apply {
+                arguments = bundle
+            }
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, updateReportFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+        getNotificationsFromFirestore()
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
